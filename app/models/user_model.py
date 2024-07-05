ff76a5ad3d567a42d4b6104ef8edc2bd37a28009
KOSASIH <kosasihg88@gmail.com>
2024-07-05 10:50:01 +0700

    Create user_model.py

---
+++ app/models/user_model.py
@@ -0,0 +1,17 @@
+from app import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), nullable=False, unique=True)
+    email = db.Column(db.String(100), nullable=False, unique=True)
+    password = db.Column(db.String(100), nullable=False)
+    role = db.Column(db.String(20), nullable=False, default='customer')
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'username': self.username,
+            'email': self.email,
+            'password': self.password,
+            'role': self.role
+        }
